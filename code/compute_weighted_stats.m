a54cdb1f08145cc4f8b3e12e30ad3a101ae026fb
Yunjeong Lee <ylee14@uw.edu>
2023-04-20 15:07:19 -0700

    initial commit

---
+++ code/compute_weighted_stats.m
@@ -0,0 +1,18 @@
+function [weighted_mean, weighted_error] = compute_weighted_stats(average, err)
+%% Find NaN weight
+ind = isnan(err);
+
+%% Remove value having NaN weight
+average(ind) = [];
+err(ind) = [];
+
+%% Define weight
+weight = 1./err.^2;
+
+%% Calculate weighted mean
+weighted_mean = sum(average .* weight);
+
+%% Calculate weighted standard error or standard deviation
+weighted_error = sqrt(sum(weight .* (average - weighted_mean).^2)/sum(weight));
+
+end
