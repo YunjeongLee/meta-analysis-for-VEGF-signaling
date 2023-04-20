beba41fe7f47016aa2d26fbb6aa6053c01c79801
Yunjeong Lee <ylee14@uw.edu>
2023-04-20 15:12:52 -0700

    correct weighted mean calculation

--- code/compute_weighted_stats.m
+++ code/compute_weighted_stats.m
@@ -10,7 +10,7 @@ err(ind) = [];
 weight = 1./err.^2;
 
 %% Calculate weighted mean
-weighted_mean = sum(average .* weight);
+weighted_mean = sum(average .* weight)/sum(weight);
 
 %% Calculate weighted standard error or standard deviation
 weighted_error = sqrt(sum(weight .* (average - weighted_mean).^2)/sum(weight));
