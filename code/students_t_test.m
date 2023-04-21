c30096835cfff7f6a41089a1a3ed8a6aa3775e53
Yunjeong Lee <ylee14@uw.edu>
2023-04-21 09:31:45 -0700

    line adjustment

--- code/students_t_test.m
+++ code/students_t_test.m
@@ -1,10 +1,10 @@
 function [tval, pval, RejectOrNot] = students_t_test(sample1, sample2, alpha, OneSideOrTwoSide)
 %% Calculate tvalue
 mean_difference = sample1.mean - sample2.mean;
-    variance = sqrt(sample1.error^2/sample1.sample_size + sample2.error^2/sample2.sample_size);
-    df = (sample1.error^2/sample1.sample_size + sample2.error^2/sample2.sample_size)^2 ...
-        / (sample1.error^4/sample1.sample_size^2/(sample1.sample_size - 1) ...
-        + sample2.error^4/sample2.sample_size^2/(sample2.sample_size - 1));
+variance = sqrt(sample1.error^2/sample1.sample_size + sample2.error^2/sample2.sample_size);
+df = (sample1.error^2/sample1.sample_size + sample2.error^2/sample2.sample_size)^2 ...
+    / (sample1.error^4/sample1.sample_size^2/(sample1.sample_size - 1) ...
+    + sample2.error^4/sample2.sample_size^2/(sample2.sample_size - 1));
 
 % Round down
 df = floor(df);
