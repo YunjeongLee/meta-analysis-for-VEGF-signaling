161273997c596608a87cdfe2534cf70bbf064fe5
Yunjeong Lee <ylee14@uw.edu>
2023-04-20 17:30:42 -0700

    initial commit

---
+++ code/students_t_test.m
@@ -0,0 +1,34 @@
+function [tval, pval, RejectOrNot] = students_t_test(sample1, sample2, alpha, OneSideOrTwoSide)
+%% Calculate tvalue
+mean_difference = sample1.mean - sample2.mean;
+if contains(who(), 'cbm')
+    % If the sample is CBM, then calculate tval differently
+    variance = sqrt(sample1.error^2/sample1.sample_size + sample2.error^2/sample2.sample_size);
+    df = (sample1.error^2/sample1.sample_size + sample2.error^2/sample2.sample_size)^2 ...
+        / (sample1.error^4/sample1.sample_size^2/(sample1.sample_size - 1) ...
+        + sample2.error^4/sample2.sample_size^2/(sample2.sample_size - 1));
+else
+    variance = sqrt(sample1.error^2 + sample2.error^2);
+    df = (sample1.error^2 + sample2.error^2)^2 ...
+        / (sample1.error^4/(sample1.sample_size - 1) + sample2.error^4/(sample2.sample_size - 1));
+end
+
+tval = mean_difference/variance;
+
+%% Find critical value
+
+if OneSideOrTwoSide == "one-side"
+    critical_val = tinv(1-alpha, df);
+elseif OneSideOrTwoSide == "two-side"
+    critical_val = tinv(1-alpha/2, df);
+else
+    sprintf('You should put either "one-side" or "two-side" into "OneSideOrTwoSide" input.');
+end
+
+pval = 1 - tcdf(tval, df);
+
+if pval <= alpha
+    RejectOrNot = "reject";
+else
+    RejectOrNot = "accept";
+end
