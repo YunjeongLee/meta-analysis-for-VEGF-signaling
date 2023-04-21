fa75c95a4a43584b21fe1d6e16d9c3e66b515e22
Yunjeong Lee <ylee14@uw.edu>
2023-04-21 09:50:57 -0700

    use `squared_se` field to calculate the standard_deviation of t-statistic

--- code/students_t_test.m
+++ code/students_t_test.m
@@ -4,7 +4,7 @@ sample1.squared_se = sample1.sd^2/sample1.size;
 sample2.squared_se = sample2.sd^2/sample2.size;
 
 mean_difference = sample1.mean - sample2.mean;
-variance = sqrt(sample1.sd^2/sample1.size + sample2.sd^2/sample2.size);
+variance = sqrt(sample1.squared_se + sample2.squared_se);
 
 % Calculate degree of freedom (df)
 df = (sample1.sd^2/sample1.size + sample2.sd^2/sample2.size)^2 ...
