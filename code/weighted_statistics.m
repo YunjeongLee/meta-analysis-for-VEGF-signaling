e52355b3af4f6b15b28b80d03354b396fdeef0b0
Yunjeong Lee <ylee14@uw.edu>
2023-04-26 14:28:23 -0700

    change font size

--- code/weighted_statistics.m
+++ code/weighted_statistics.m
@@ -64,7 +64,7 @@ figure('pos', [10 10 1200 500]);
 sgtitle('Geometric parameters', 'fontsize', 20)
 
 subplot(1, 2, 1);
-set(gca, 'fontsize', 13);
+set(gca, 'fontsize', 18);
 title("Adipocyte size (µm)", " ")
 hold on;
 lean1 = plot(1, table_adip_size{"Morange2000", "Lean average"}, 'ko', 'Markersize', 10, 'MarkerFaceColor', 'none', 'linewidth', 1.5);
@@ -79,7 +79,7 @@ obese4 = plot(2, table_adip_size{"Voros2005", "Obese average"}, 'k^', 'Markersiz
 obese5 = plot(2, table_adip_size{"Lijnen2006", "Obese average"}, 'kdiamond', 'Markersize', 10, 'MarkerFaceColor', 'none', 'linewidth', 1.5);
 obese6 = plot(2, adip_obese.mean, 'LineStyle', 'none', 'Marker', '_', 'Markersize', 10, 'MarkerEdgeColor', 'r', 'linewidth', 1.5);
 text(2, adip_obese.mean, ' *', 'color', 'r', 'fontsize', 20)
-text(2, adip_obese.mean, sprintf('    p=%.4f', pval_adip), 'color', 'r', 'fontsize', 12)
+text(2, adip_obese.mean, sprintf('   p=%.3f', pval_adip), 'color', 'r', 'fontsize', 16)
 hold off;
 xlim([0, 3])
 ylim([0, 100])
@@ -90,7 +90,7 @@ lgd1 = legend([lean1, lean2, lean3, lean4, lean5], ...
     'Location', 'northeastoutside');
 title(lgd1, "Lean mouse")
 a = axes('position',get(gca,'position'),'visible','off');
-set(gca, 'fontsize', 13);
+set(gca, 'fontsize', 18);
 lgd2 = legend(a, [obese1, obese2, obese3, obese4, obese5, obese6], ...
     {"Morange, 2000"; "Lijnen, 2001"; "Maquoi, 2002"; "Voros, 2005"; "Lijnen, 2006"; "Weighted average"}, ...
     'Location', 'Eastoutside');
@@ -98,7 +98,7 @@ title(lgd2, "Obese mouse")
 
 subplot(1, 2, 2);
 title("Capillary basement membrane thickness (nm)", " ")
-set(gca, 'fontsize', 13);
+set(gca, 'fontsize', 18);
 hold on;
 lean1 = plot(1, table_cbm{"Creutzfeldt", "Lean average"}, 'ko', 'Markersize', 10, 'MarkerFaceColor', 'none', 'linewidth', 1.5);
 lean2 = plot(1, table_cbm{"Rodrigues1983", "Lean average"}, 'ksquare', 'Markersize', 10, 'MarkerFaceColor', 'none', 'linewidth', 1.5);
@@ -125,7 +125,7 @@ lgd1 = legend([lean1, lean2, lean3, lean4, lean5, lean6, lean7, lean8, lean9], .
     'Location', 'northeastoutside');
 title(lgd1, "Lean mouse")
 a = axes('position',get(gca,'position'),'visible','off');
-set(gca, 'fontsize', 13);
+set(gca, 'fontsize', 18);
 lgd2 = legend(a, [obese1, obese2, obese3, obese4], ...
     {"Lash, 1989 (11 wk.)"; "Lash, 1989 (18 wk.)"; "Danis, 1993"; "Weighted average"}, ...
     'Location', 'eastoutside');
@@ -142,7 +142,7 @@ sgtitle('Binding affinities of VEGF to its receptors', 'fontsize', 20)
 
 subplot(1, 3, 1);
 title("Binding affinity of VEGF to VEGFR1 (pM)", " ")
-set(gca, 'fontsize', 13, 'yscale', 'log');
+set(gca, 'fontsize', 18, 'yscale', 'log');
 hold on;
 radio1 = plot(1, table_vegfr1{"Waltenberger1994", "Kd average"}, 'ko', 'Markersize', 10, 'MarkerFaceColor', 'none', 'linewidth', 1.5);
 radio2 = plot(1, table_vegfr1{"Waltenberger1994_1", "Kd average"}, 'ksquare', 'Markersize', 10, 'MarkerFaceColor', 'none', 'linewidth', 1.5);
@@ -161,7 +161,7 @@ lgd1 = legend([radio1, radio2, radio3], ...
     'Location', 'northeastoutside');
 title(lgd1, "Radioligand")
 a = axes('position',get(gca,'position'),'visible','off');
-set(gca, 'fontsize', 13);
+set(gca, 'fontsize', 18);
 lgd2 = legend(a, [spr1, spr2, spr3], ...
     {"Tiedemann, 2002"; "Mamer, 2020"; "Weighted average"}, ...
     'Location', 'Eastoutside');
@@ -169,7 +169,7 @@ title(lgd2, "SPR")
 
 subplot(1, 3, 2);
 title("Binding affinity of VEGF to VEGFR2 (pM)", " ")
-set(gca, 'fontsize', 13, 'yscale', 'log');
+set(gca, 'fontsize', 18, 'yscale', 'log');
 hold on;
 radio1 = plot(1, table_vegfr2{"Waltenberger1994", "Kd average"}, 'ko', 'Markersize', 10, 'MarkerFaceColor', 'none', 'linewidth', 1.5);
 radio2 = plot(1, table_vegfr2{"Waltenberger1994_1", "Kd average"}, 'ksquare', 'Markersize', 10, 'MarkerFaceColor', 'none', 'linewidth', 1.5);
@@ -182,7 +182,7 @@ spr4 = plot(2, table_vegfr2{"Mamer2020", "Kd average"}, 'kpentagram', 'Markersiz
 spr5 = plot(2, table_vegfr2{"Shobhan2023", "Kd average"}, 'kx', 'Markersize', 10, 'MarkerFaceColor', 'none', 'linewidth', 1.5);
 spr6 = plot(2, vegfr2_spr.mean, 'LineStyle', 'none', 'Marker', '_', 'Markersize', 10, 'MarkerEdgeColor', 'r', 'linewidth', 1.5);
 text(2, vegfr2_spr.mean, ' *', 'color', 'r', 'fontsize', 20)
-text(2, vegfr2_spr.mean, sprintf('    p=%.4f', pval_vegfr2), 'color', 'r', 'fontsize', 12)
+text(2, vegfr2_spr.mean, sprintf('   p=%.3f', pval_vegfr2), 'color', 'r', 'fontsize', 16)
 hold off;
 xlim([0, 3])
 ylim([1e-1, 1e3])
@@ -193,7 +193,7 @@ lgd1 = legend([radio1, radio2, radio3, radio4], ...
     'Location', 'northeastoutside');
 title(lgd1, "Radioligand")
 a = axes('position',get(gca,'position'),'visible','off');
-set(gca, 'fontsize', 13);
+set(gca, 'fontsize', 18);
 lgd2 = legend(a, [spr1, spr2, spr3, spr4, spr5, spr6], ...
     {"Huang, 1998"; "Cunningham, 1999" + newline + "(pre-dimer)"; "Cunningham, 1999" + newline + "(monomer)"; "Mamer, 2020"; "Shobhan, 2023" + newline + "(unpublished)"; "Weighted average"}, ...
     'Location', 'eastoutside');
@@ -201,7 +201,7 @@ title(lgd2, "SPR")
 
 subplot(1, 3, 3);
 title("Binding affinity of VEGF to NRP1 (nM)", " ")
-set(gca, 'fontsize', 13, 'yscale', 'log');
+set(gca, 'fontsize', 18, 'yscale', 'log');
 hold on;
 radio1 = plot(1, table_nrp1{"Soker1996", "Kd average"}, 'ko', 'Markersize', 10, 'MarkerFaceColor', 'none', 'linewidth', 1.5);
 radio2 = plot(1, table_nrp1{"Soker1996_1", "Kd average"}, 'ksquare', 'Markersize', 10, 'MarkerFaceColor', 'none', 'linewidth', 1.5);
@@ -213,7 +213,7 @@ spr2 = plot(2, table_nrp1{"Teran2019", "Kd average"}, 'ksquare', 'Markersize', 1
 spr3 = plot(2, table_nrp1{"Teran2019", "Kd average"}, 'k^', 'Markersize', 10, 'MarkerFaceColor', 'none', 'linewidth', 1.5);
 spr4 = plot(2, nrp1_spr.mean, 'LineStyle', 'none', 'Marker', '_', 'Markersize', 10, 'MarkerEdgeColor', 'r', 'linewidth', 1.5);
 text(2, nrp1_spr.mean, ' *', 'color', 'r', 'fontsize', 20)
-text(2, nrp1_spr.mean, sprintf('    p=%.4f', pval_nrp1), 'color', 'r', 'fontsize', 12)
+text(2, nrp1_spr.mean, sprintf('   p=%.3f', pval_nrp1), 'color', 'r', 'fontsize', 16)
 hold off;
 xlim([0, 3])
 ylim([1e-1, 1e3])
@@ -224,7 +224,7 @@ lgd1 = legend([radio1, radio2, radio3, radio4, radio5], ...
     'Location', 'northeastoutside');
 title(lgd1, "Radioligand")
 a = axes('position',get(gca,'position'),'visible','off');
-set(gca, 'fontsize', 13);
+set(gca, 'fontsize', 18);
 lgd2 = legend(a, [spr1, spr2, spr3, spr4], ...
     {"Fuh, 2000"; "Teran, 2019" + newline + "(chimera)"; "Teran, 2019" + newline + "(monomer)"; "Weighted average"}, ...
     'Location', 'eastoutside');
