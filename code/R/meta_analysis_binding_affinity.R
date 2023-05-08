00f951373abaa6e1869c157e07a7edc25adddce5
Yunjeong Lee <ylee14@uw.edu>
2023-05-08 12:16:46 -0700

    save forest figures using `png()` function

--- code/R/meta_analysis_binding_affinity.R
+++ code/R/meta_analysis_binding_affinity.R
@@ -71,12 +71,15 @@ rm_nrp1 <- rma(yi = Average, sei = SE, data=nrp1)
 summary(rm_nrp1)
 
 # Forest plot -------------------------------------------------------------
+png(file=sprintf("%s/forest_vegfr1.png", results_path), width=1300, height=500)
 forest(rm_vegfr1, slab=vegfr1$Reference, header=TRUE,
        xlab="", xlim = c(-0.02, 0.07), alim = c(0, 0.05), cex=2)
 mtext(side=1, "Binding affinity, Kd (nM)", padj=2, cex = 2, line=1)
+png(file=sprintf("%s/forest_vegfr2.png", results_path), width=1300, height=700)
 forest(rm_vegfr2, slab=vegfr2$Reference, header=TRUE, 
        xlab="", xlim = c(-0.5, 1.5), alim = c(0, 1), cex=2)
 mtext(side=1, "Binding affinity, Kd (nM)", padj=2, cex = 2, line=1)
+png(file=sprintf("%s/forest_nrp1.png", results_path), width=1300, height=700)
 forest(rm_nrp1, slab=nrp1$Reference, header=TRUE, 
        xlab="", xlim = c(-70, 300), alim = c(0, 200), cex=2)
 mtext(side=1, "Binding affinity, Kd (nM)", padj=2, cex = 2, line=1)
