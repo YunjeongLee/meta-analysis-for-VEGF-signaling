b5af579770893b6ddfa81b40819c0c84eb098195
Yunjeong Lee <ylee14@uw.edu>
2023-05-26 15:37:23 -0700

    change height of forest plot for VEGF:VEGFR2 (Radioligand)

--- code/R/compare_spr_radioligand.R
+++ code/R/compare_spr_radioligand.R
@@ -121,7 +121,7 @@ mtext(side=1, TeX("Binding affinity, Kd (pM)"), padj=2, cex = 2, line=1)
 dev.off()
 
 # VEGF:VEGFR2 (Radioligand)
-png(file=sprintf("%s/forest_vegfr2_radio.png", results_path), width=1300, height=700)
+png(file=sprintf("%s/forest_vegfr2_radio.png", results_path), width=1300, height=500)
 forest(rm_vegfr2_radio, slab=vegfr2_radio$Reference, header=TRUE,
        xlab="", xlim = c(-60, 180), alim = c(20, 120), cex=2)
 mtext(side=1, TeX("Binding affinity, Kd (pM)"), padj=2, cex = 2, line=1)
