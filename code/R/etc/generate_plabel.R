65fd6cb94ba683feb993977d6a034b941abefaa0
Yunjeong Lee <ylee14@uw.edu>
2023-05-26 13:28:28 -0700

    edit condition from `pval <= 0.01` to `0.001 < pval <= 0.01`

--- code/R/etc/generate_plabel.R
+++ code/R/etc/generate_plabel.R
@@ -2,7 +2,7 @@ generate_plabel <- function(pval) {
   if ((pval > 0.01) & (pval <= 0.05)) {
     res <- "p < 0.05"
   }
-  else if (pval <= 0.01) {
+  else if ((pval > 0.001) & (pval <= 0.01)) {
     res <- "p < 0.01"
   }
   return(res)
