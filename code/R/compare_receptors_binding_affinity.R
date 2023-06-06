b454dd5e35de33211bf2e1c159506bfd73019fec
Yunjeong Lee <ylee14@uw.edu>
2023-06-06 17:02:05 -0400

    if there is no SE, then use [Average x 0.1] as SE

--- code/R/compare_receptors_binding_affinity.R
+++ code/R/compare_receptors_binding_affinity.R
@@ -52,7 +52,7 @@ nrp1 <- nrp1[, c("Reference", "Average", "SE")]
 vegfr2 <- vegfr2[!is.na(vegfr2$Reference), ]
 
 # Deal with missing standard error ----------------------------------------
-vegfr1[is.na(vegfr1$SE), "SE"] <- 0.1*(vegfr1[is.na(vegfr1$SE), "Average"])
+vegfr1[is.na(vegfr1["SE"]), "SE"] <- vegfr1[is.na(vegfr1["SE"]), "Average"] * 0.1
 vegfr2[is.na(vegfr2["SE"]), "SE"] <- vegfr2[is.na(vegfr2["SE"]), "Average"] * 0.1
 nrp1[is.na(nrp1["SE"]), "SE"] <- nrp1[is.na(nrp1["SE"]), "Average"] * 0.1
 
