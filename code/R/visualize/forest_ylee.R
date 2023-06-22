995847a3f8aad657dfcabd8c4059cfa97e298c19
Yunjeong Lee <ylee14@uw.edu>
2023-06-22 10:44:28 -0700

    apply `atransf` to `forest`

--- code/R/visualize/forest_ylee.R
+++ code/R/visualize/forest_ylee.R
@@ -21,7 +21,7 @@ forest_ylee <- function (data, rm, slab, xlim, alim, unit, xlab, cex=2, atransf=
   ### forest plot with extra annotations
   sav <- forest(rm, slab=slab, 
                 header=c("Author(s) and Year", ""), 
-                xlim=xlim, alim=alim, cex=cex,
+                xlim=xlim, alim=alim, cex=cex, atransf=atransf,
                 ilab=weights, ilab.xpos=ilab_pos, ilab.pos=2,
                 xlab=xlab, mlab="Random-effects model", refline=NA, pch=18, psize=psize,
                 colout=linecolor, col=linecolor, border=linecolor, lwd=4)
