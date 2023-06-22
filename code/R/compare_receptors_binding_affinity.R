e3098302c88f11b13bdbd2a57b5bd0b46c36e2a9
Yunjeong Lee <ylee14@uw.edu>
2023-06-22 13:54:33 -0700

    use `guides` to order legends

--- code/R/compare_receptors_binding_affinity.R
+++ code/R/compare_receptors_binding_affinity.R
@@ -128,11 +128,13 @@ p = ggplot() +
   geom_point(data = vegfr2, aes(x = "VEGFR2", y=rm_vegfr2$b*1e3), shape = 95, size=20, colour = "darkgreen") +
   labs(color="VEGFR2") +
   lightness(scale_color_colormap('VEGFR2', discrete = T,colormap = "greens", reverse = T), scalefac(0.8)) + 
+  guides(color = guide_legend(order=2)) +
   new_scale_color() +
   geom_point(data = nrp1, aes(x = "NRP1", y = Average*1e3, colour = Reference), size = 7) +
   geom_point(data = nrp1, aes(x = "NRP1", y=rm_nrp1$b*1e3), shape = 95, size=20, colour = "darkred") +
   labs(color="NRP1") +
   lightness(scale_color_brewer(palette="Oranges"),scalefac(0.8)) +
+  guides(color = guide_legend(order=3)) +
   xlab("") + ylab(TeX("Binding affinity, Kd (pM)")) +
   scale_y_continuous(trans= 'log10', breaks=trans_breaks('log10', function(x) 10^x),
                      labels=trans_format('log10', math_format(10^.x)), limits = c(1e-1, 1e7),
