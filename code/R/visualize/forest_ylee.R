734c6e5259e34d69ada6813688456b984d6b601f
Yunjeong Lee <ylee14@uw.edu>
2023-06-29 10:38:16 -0700

    add title to `forest_ylee` as an input

--- code/R/visualize/forest_ylee.R
+++ code/R/visualize/forest_ylee.R
@@ -1,4 +1,4 @@
-forest_ylee <- function (data, rm, slab, xlim, alim, unit, xlab, cex=2, atransf=NULL) {
+forest_ylee <- function (data, rm, title=NULL, slab, xlim, alim, unit, xlab, cex=2, atransf=NULL) {
   ### Specify colors
   linecolor = "#1e81b0"
   
