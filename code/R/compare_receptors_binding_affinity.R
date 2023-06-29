2c1002dc08e2acbc275449ed03187a1ba0bb9f63
Yunjeong Lee <ylee14@uw.edu>
2023-06-29 10:44:23 -0700

    edit `xlim` and `alim` in the forest plot

--- code/R/compare_receptors_binding_affinity.R
+++ code/R/compare_receptors_binding_affinity.R
@@ -78,17 +78,17 @@ summary(rm_nrp1)
 png(file=sprintf("%s/forest_vegfr1.png", results_path), width=1300, height=700)
 forest_ylee(data=vegfr1, rm=rm_vegfr1, slab=vegfr1$Reference,
             unit="pM", atransf=function(x)1e3*x,
-            xlab="Binding affinity, Kd (pM)", xlim = c(-0.2, 0.45), alim = c(0, 0.25), cex=2)
+            xlab="Binding affinity, Kd (pM)", xlim = c(-0.15, 0.27), alim = c(0, 0.15), cex=2)
 dev.off()
 png(file=sprintf("%s/forest_vegfr2.png", results_path), width=1300, height=700)
 forest_ylee(data=vegfr2, rm=rm_vegfr2, slab=vegfr2$Reference, 
             unit="nM",
-            xlab="Binding affinity, Kd (nM)", xlim = c(-12, 18), alim = c(0, 10), cex=2)
+            xlab="Binding affinity, Kd (nM)", xlim = c(-1.2, 1.8), alim = c(0, 1), cex=2)
 dev.off()
 png(file=sprintf("%s/forest_nrp1.png", results_path), width=1300, height=700)
 forest_ylee(data=nrp1, rm=rm_nrp1, slab=nrp1$Reference, 
             unit="nM",
-            xlab="Binding affinity, Kd (nM)", xlim = c(-20, 50), alim = c(0, 30), cex=2)
+            xlab="Binding affinity, Kd (nM)", xlim = c(-25, 50), alim = c(0, 30), cex=2)
 dev.off()
 
 # Student's t-test --------------------------------------------------------
