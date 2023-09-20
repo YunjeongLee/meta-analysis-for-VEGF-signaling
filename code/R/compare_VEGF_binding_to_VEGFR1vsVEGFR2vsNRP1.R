cfbaf497234369ec92380a2e202dee55afc4b8ca
Ara101 <Lionel.tukei@gmail.com>
2023-09-19 17:07:48 -0700

    Update compare_VEGF_binding_to_VEGFR1vsVEGFR2vsNRP1.R

--- code/R/compare_VEGF_binding_to_VEGFR1vsVEGFR2vsNRP1.R
+++ code/R/compare_VEGF_binding_to_VEGFR1vsVEGFR2vsNRP1.R
@@ -31,7 +31,11 @@ instant_pkgs(pkg_list)
 
 # Load data ---------------------------------------------------------------
 filename = '../../data/kon_koffs.xlsx'
+filename_fitted = '../../data/VEGFA165_VEGFR2_NRP1_fitted_binding_kinetics.xlsx'
+
 vegfkonkoff <- as.data.frame(read_excel(filename))
+
+vegfkonkoff_fitted <- as.data.frame(read_excel(filename_fitted))
 
 # Split Data by parameter -------------------------------------------------
 
@@ -39,6 +43,11 @@ VEGF165VEGFR1 <- vegfkonkoff[vegfkonkoff$Parameter == "VEGF165:VEGR1", ]
 
 VEGF165VEGFR2 <- vegfkonkoff[vegfkonkoff$Parameter == "VEGF165:VEGFR2", ]
 
+VEGF165NRP1 <- vegfkonkoff[vegfkonkoff$Parameter == "VEGF165:NRP1", ]
+
+VEGF165VEGFR2_fitted <- vegfkonkoff_fitted[vegfkonkoff_fitted$Parameter == "VEGF165:VEGFR2", ]
+
+VEGF165NRP1_fitted <- vegfkonkoff_fitted[vegfkonkoff_fitted$Parameter == "VEGF165:NRP1", ]
 
 # Give assumed SE----------------------------------------------------------
 
@@ -65,6 +74,9 @@ VEGF165VEGFR2 <- update_se(VEGF165VEGFR2, "Kd", "Kd_SE", "VEGF165")
 VEGF165VEGFR2 <- update_se(VEGF165VEGFR2, "kon", "kon_SE", "VEGF164")
 VEGF165VEGFR2 <- update_se(VEGF165VEGFR2, "koff", "koff_SE", "VEGF164")
 VEGF165VEGFR2 <- update_se(VEGF165VEGFR2, "Kd", "Kd_SE", "VEGF164")
+VEGF165NRP1 <- update_se(VEGF165NRP1,  "kon", "kon_SE", "VEGF165")
+VEGF165NRP1 <- update_se(VEGF165NRP1,  "koff", "koff_SE", "VEGF165")
+VEGF165NRP1 <- update_se(VEGF165NRP1,  "Kd", "Kd_SE", "VEGF165")
 
 # Change reference names to make them distinct
 
@@ -72,6 +84,9 @@ VEGF165VEGFR2[VEGF165VEGFR2$Ligand == "VEGF165" & VEGF165VEGFR2$Reference == "Hu
 VEGF165VEGFR2[VEGF165VEGFR2$Ligand == "VEGF164" & VEGF165VEGFR2$Reference == "Huang et al., 1998", "Reference"] <- "Huang et al., 1998 (VEGF-A164)"
 VEGF165VEGFR2[VEGF165VEGFR2$kon == 3600000 & VEGF165VEGFR2$Reference == "Cunningham et al., 1999", "Reference"] <- "Cunningham et al., 1999 (predimer)"
 VEGF165VEGFR2[VEGF165VEGFR2$kon == 5230000 & VEGF165VEGFR2$Reference == "Cunningham et al., 1999", "Reference"] <- "Cunningham et al., 1999 (monomer)"
+
+VEGF165NRP1[VEGF165NRP1$kon == 581000 & VEGF165NRP1$Reference == "Inhouse Data 2023", "Reference"] <- "Inhouse Data 2023 (Trial 1)"
+VEGF165NRP1[VEGF165NRP1$kon == 1010000 & VEGF165NRP1$Reference == "Inhouse Data 2023", "Reference"] <- "Inhouse Data 2023 (Trial 2)"
 
 # combined the rows and display the means 
 
@@ -83,15 +98,30 @@ VEGF165VEGFR2[VEGF165VEGFR2$Reference == "Huang et al., 1998 (VEGF-A165)", "Kd"]
 VEGF165VEGFR2[VEGF165VEGFR2$Reference == "Huang et al., 1998 (VEGF-A164)", "Kd"] <- mean(VEGF165VEGFR2[VEGF165VEGFR2$Reference == "Huang et al., 1998 (VEGF-A164)", "Kd"])
 VEGF165VEGFR2 <- unique(VEGF165VEGFR2)
 
+VEGF165NRP1[VEGF165NRP1$Reference == "Inhouse Data 2023 (Trial 1)", "kon"] <- mean(VEGF165NRP1[VEGF165NRP1$Reference == "Inhouse Data 2023 (Trial 1)", "kon"])
+VEGF165NRP1[VEGF165NRP1$Reference == "Inhouse Data 2023 (Trial 2)", "kon"] <- mean(VEGF165NRP1[VEGF165NRP1$Reference == "Inhouse Data 2023 (Trial 2)", "kon"])
+VEGF165NRP1[VEGF165NRP1$Reference == "Inhouse Data 2023 (Trial 1)", "koff"] <- mean(VEGF165NRP1[VEGF165NRP1$Reference == "Inhouse Data 2023 (Trial 1)", "koff"])
+VEGF165NRP1[VEGF165NRP1$Reference == "Inhouse Data 2023 (Trial 2)", "koff"] <- mean(VEGF165NRP1[VEGF165NRP1$Reference == "Inhouse Data 2023 (Trial 2)", "koff"])
+VEGF165NRP1[VEGF165NRP1$Reference == "Inhouse Data 2023 (Trial 1)", "Kd"] <- mean(VEGF165NRP1[VEGF165NRP1$Reference == "Inhouse Data 2023 (Trial 1)", "Kd"])
+VEGF165NRP1[VEGF165NRP1$Reference == "Inhouse Data 2023 (Trial 2)", "Kd"] <- mean(VEGF165NRP1[VEGF165NRP1$Reference == "Inhouse Data 2023 (Trial 2)", "Kd"])
+VEGF165NRP1 <- unique(VEGF165NRP1)
+
+# Add the new data 
+
+VEGF165NRP1 <- rbind(VEGF165NRP1, VEGF165NRP1_fitted)
+VEGF165VEGFR2 <- rbind(VEGF165VEGFR2, VEGF165VEGFR2_fitted)
+
 # Rename variables
 
 vegfr1 <- VEGF165VEGFR1
 vegfr2 <- VEGF165VEGFR2
+nrp1 <- VEGF165NRP1
 
 # Vegfr1 kon mean and kon se divide all values by 1e6
 
 vegfr1[c("kon", "kon_SE")] <- vegfr1[c("kon", "kon_SE")]/1e6
 vegfr2[c("kon", "kon_SE")] <- vegfr2[c("kon", "kon_SE")]/1e6
+nrp1[c("kon", "kon_SE")] <- nrp1[c("kon", "kon_SE")]/1e6
 
 # Meta-analysis -----------------------------------------------------------
 # Compute weighted average and SD -----------------------------------------
@@ -103,6 +133,10 @@ summary(rm_vegfr1_kon)
 rm_vegfr2_kon <- rma(yi = kon, sei = kon_SE, data = vegfr2)
 summary(rm_vegfr2_kon)
 
+# VEGF-A165:NRP1 kon
+rm_nrp1_koff <- rma(yi = koff, sei = koff_SE, data=nrp1)
+summary(rm_nrp1_koff)
+
 # VEGF-A165:VEGFR1 koff
 rm_vegfr1_koff <- rma(yi = koff, sei = koff_SE, data=vegfr1)
 summary(rm_vegfr1_koff)
@@ -110,6 +144,10 @@ summary(rm_vegfr1_koff)
 # VEGF-A165:VEGFR2 koff
 rm_vegfr2_koff <- rma(yi = koff, sei = koff_SE, data=vegfr2)
 summary(rm_vegfr2_koff)
+
+# VEGF-A165:NRP1 koff
+rm_vegfr1_koff <- rma(yi = koff, sei = koff_SE, data=nrp1)
+summary(rm_nrp1_koff)
 
 # Forest plot -------------------------------------------------------------
 # kon
@@ -119,10 +157,18 @@ forest_ylee(data=vegfr1, rm=rm_vegfr1_kon, slab=vegfr1$Reference,
             title="Association rates of VEGF-A165:VEGFR1",
             xlab=TeX("Association rate, $k_{on}$ ($\\mu M^{-1} s^{-1}$)"), xlim = c(-40, 75), alim = c(0, 45), cex=2)
 dev.off()
+
 png(file=sprintf("%s/forest_vegfr2vegf165_kon.png", results_path), width=1300, height=500)
 forest_ylee(data=vegfr2, rm=rm_vegfr2_kon, slab=vegfr2$Reference, 
             unit = stri_unescape_unicode(gsub("<U\\+(....)>", "\\\\u\\1", "<U+00B5>M<U+207B><U+00B9> s<U+207B><U+00B9>")),
             title="Association rates of VEGF-A165:VEGFR2",
+            xlab=TeX("Association rate, $k_{on}$ ($\\mu M^{-1} s^{-1}$)"), xlim = c(-0.1e+2, 1.6e+1), alim = c(0, .9e+1), cex=2)
+dev.off()
+
+png(file=sprintf("%s/forest_nrp1vegf165_kon.png", results_path), width=1300, height=500)
+forest_ylee(data=nrp1, rm=rm_vegfr2_kon, slab=vegfr2$Reference, 
+            unit = stri_unescape_unicode(gsub("<U\\+(....)>", "\\\\u\\1", "<U+00B5>M<U+207B><U+00B9> s<U+207B><U+00B9>")),
+            title="Association rates of VEGF-A165:NRP1",
             xlab=TeX("Association rate, $k_{on}$ ($\\mu M^{-1} s^{-1}$)"), xlim = c(-0.1e+2, 1.6e+1), alim = c(0, .9e+1), cex=2)
 dev.off()
 
@@ -137,6 +183,12 @@ png(file=sprintf("%s/forest_vegfr2vegf165_koff.png", results_path), width=1300, 
 forest_ylee(data=vegfr2, rm=rm_vegfr2_koff, slab=vegfr2$Reference, 
             unit = paste0("s", stri_unescape_unicode(gsub("<U\\+(....)>", "\\\\u\\1", "<U+207B><U+00B9>"))),
             title="Dissociation rates of VEGF-A165:VEGFR2",
+            xlab=TeX("Dissociation rate, $k_{off}$ ($\\times 10^{-3} s^{-1}$)"), xlim = c(-1e-3, 1.7e-3), alim = c(0, 1e-3), cex=2, atransf=function(x) x*1e3)
+dev.off()
+png(file=sprintf("%s/forest_nrp1vegf165_koff.png", results_path), width=1300, height=500)
+forest_ylee(data=nrp1, rm=rm_vegfr2_koff, slab=vegfr2$Reference, 
+            unit = paste0("s", stri_unescape_unicode(gsub("<U\\+(....)>", "\\\\u\\1", "<U+207B><U+00B9>"))),
+            title="Dissociation rates of VEGF-A165:nrp1",
             xlab=TeX("Dissociation rate, $k_{off}$ ($\\times 10^{-3} s^{-1}$)"), xlim = c(-1e-3, 1.7e-3), alim = c(0, 1e-3), cex=2, atransf=function(x) x*1e3)
 dev.off()
 
@@ -154,10 +206,24 @@ vegfr1_vs_vegfr2koff = wtd.t.test(x=vegfr1$koff, y=vegfr2$koff,
                                   weighty=1/(vegfr2$koff_SE^2+rm_vegfr2_koff$tau2),
                                   alternative="less", samedata=FALSE)
 
+# kon Comparison nrp1
+nrp1_vs_vegfr2kon = wtd.t.test(x=nrp1$kon, y=vegfr2$kon,
+                                 weight=1/(nrp1$kon_SE^2+rm_nrp1_kon$tau2), 
+                                 weighty=1/(vegfr2$kon_SE^2+rm_vegfr2_kon$tau2),
+                                 alternative="greater", samedata=FALSE)
+
+
+# koff Comparison nrp1
+nrp1_vs_vegfr1koff = wtd.t.test(x=vegfr1$koff, y=nrp1$koff,
+                                  weight=1/(vegfr1$koff_SE^2+rm_vegfr1_koff$tau2), 
+                                  weighty=1/(nrp1$koff_SE^2+rm_nrp1_koff$tau2),
+                                  alternative="less", samedata=FALSE)
+
 
 # Merge dataframes for plotting -------------------------------------------
 vegfr1$Ligand <- "VEGFR1"
 vegfr2$Ligand <- "VEGFR2"
+nrp1$Ligand <- "NRP1"
 
 df = rbind(vegfr1[c("Ligand", "kon")],
            vegfr2[c("Ligand", "kon")])
